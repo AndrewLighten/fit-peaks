80957eb408c759d823dc33a71422721d01218801
Andrew Lighten <andrew@digital-ironworks.com>
2020-02-19 11:35:08 +1100

    break detail report into labelled sections
    - Now uses ANSI escape sequences to label different sections of the
      detail report

--- detail.py
+++ detail.py
@@ -33,20 +33,22 @@ def _print_basic_data(activity: Activity):
     """
 
     # Activity ID and name
-    print()
-    print(f"Activity #{activity.rowid}: {activity.activity_name}")
+    print("\033[2J\033[H")
+    print(f"\fActivity #{activity.rowid}: \033[32m\033[1m{activity.activity_name}\033[0m")
 
     # Date, time, duration
-    date = activity.start_time.strftime("%a %d %b, %Y")
-    start = activity.start_time.strftime("%H:%M")
-    end = activity.end_time.strftime("%H:%M")
+    date = activity.start_time.strftime("%A %d %B, %Y")
+    start = activity.start_time.strftime("%H:%M:%S")
+    end = activity.end_time.strftime("%H:%M:%S")
     duration = str(activity.end_time - activity.start_time).rjust(8)
 
     print()
-    print(f"Date ................ {date}")
-    print(f"Start time .......... {start}")
-    print(f"Duration ............ {duration}")
-    print(f"End time ............ {end}")
+    print("\033[34m\033[1mBasic statistics\033[0m")
+    print("")
+    print(f"    Date ................ {date}")
+    print(f"    Start time .......... {start}")
+    print(f"    Duration ............ {duration}")
+    print(f"    End time ............ {end}")
 
     # Distances
     distance = format(round(activity.distance / 1000, 2), ".2f") + "km"
@@ -58,9 +60,9 @@ def _print_basic_data(activity: Activity):
     speed_in_kmhr = format(speed_in_ms * 3600 / 1000, ".1f")
 
     print()
-    print(f"Distance ............ {distance}")
-    print(f"Average speed ....... {speed_in_kmhr} km/hr")
-    print(f"Elevation gain ...... {elevation}")
+    print(f"    Distance ............ {distance}")
+    print(f"    Average speed ....... {speed_in_kmhr}km/hr")
+    print(f"    Elevation gain ...... {elevation}")
 
 
 def _print_power_data(activity: Activity):
@@ -74,22 +76,26 @@ def _print_power_data(activity: Activity):
     # Calculate some power details
     # Taken from https://medium.com/critical-powers/formulas-from-training-and-racing-with-a-power-meter-2a295c661b46
     ftp = get_ftp(activity.start_time)
-    variability_index = format(activity.normalised_power / activity.avg_power, ".2f")
-    intensity_factor = activity.normalised_power / ftp
-    intensity_factor_text = format(intensity_factor, ".2f")
+    variability_index = activity.normalised_power / activity.avg_power
+    intensity_factor = activity.normalised_power / ftp if ftp else 0
     duration_in_seconds = (activity.end_time - activity.start_time).seconds
-    tss = (duration_in_seconds * activity.normalised_power * intensity_factor) / (ftp * 36)
+    tss = ((duration_in_seconds * activity.normalised_power * intensity_factor) / (ftp * 36)) if ftp else 0
+
+    variability_index_text = format(variability_index, ".2f")
+    intensity_factor_text = format(intensity_factor, ".2f")
     tss_text = format(tss, ".0f")
 
     print("")
-    print(f"Average power ....... {int(activity.avg_power)}W")
-    print(f"Maximum power ....... {activity.max_power}W")
-    print(f"Normalised power .... {int(activity.normalised_power)}W")
-    print(f"Variability index ... {variability_index}")
+    print("\033[34m\033[1mPower data\033[0m")
     print("")
-    print(f"FTP ................. {ftp}W")
-    print(f"Intensity factor .... {intensity_factor_text}")
-    print(f"TSS ................. {tss_text}")
+    print(f"    Average power ....... {int(activity.avg_power)}W")
+    print(f"    Maximum power ....... {activity.max_power}W")
+    print(f"    Normalised power .... {int(activity.normalised_power)}W")
+    print(f"    Variability index ... {variability_index_text}")
+    print("")
+    print(f"    FTP ................. {ftp}W")
+    print(f"    Intensity factor .... {intensity_factor_text}")
+    print(f"    TSS ................. {tss_text}")
 
 
 def _print_hr_data(activity: Activity):
@@ -100,8 +106,10 @@ def _print_hr_data(activity: Activity):
         activity: The activity to print HR data for.
     """
     print("")
-    print(f"Average HR .......... {int(activity.avg_hr)} bpm")
-    print(f"Maximum HR .......... {activity.max_hr} bpm")
+    print("\033[34m\033[1mHeart data\033[0m")
+    print("")
+    print(f"    Average HR .......... {int(activity.avg_hr)} bpm")
+    print(f"    Maximum HR .......... {activity.max_hr} bpm")
 
 
 def _print_peaks(activity: Activity):
@@ -137,21 +145,23 @@ def _print_peaks(activity: Activity):
     hr120min = str(activity.peak_120min_hr).rjust(9) if activity.peak_120min_hr else "         "
 
     print()
+    print("\033[34m\033[1mPeaks\033[0m")
+    print("")
     print("           Power (W)   HR (bpm)")
     print("           ---------  ---------")
-    print(f"5 sec      {p5sec}  {hr5sec}")
-    print(f"30 sec     {p30sec}  {hr30sec}")
-    print(f"60 sec     {p60sec}  {hr60sec}")
-    print(f"5 min      {p5min}  {hr5min}")
-    print(f"10 min     {p10min}  {hr10min}")
+    print(f"    5 sec  {p5sec}  {hr5sec}")
+    print(f"    30 sec {p30sec}  {hr30sec}")
+    print(f"    60 sec {p60sec}  {hr60sec}")
+    print(f"    5 min  {p5min}  {hr5min}")
+    print(f"    10 min {p10min}  {hr10min}")
     if activity.peak_20min_power or activity.peak_20min_hr:
-        print(f"20 min     {p20min}  {hr20min}")
+        print(f"    20 min {p20min}  {hr20min}")
     if activity.peak_30min_power or activity.peak_30min_hr:
-        print(f"30 min     {p30min}  {hr30min}")
+        print(f"    30 min {p30min}  {hr30min}")
     if activity.peak_60min_power or activity.peak_60min_hr:
-        print(f"60 min     {p60min}  {hr60min}")
+        print(f"    60 min {p60min}  {hr60min}")
     if activity.peak_90min_power or activity.peak_90min_hr:
-        print(f"90 min     {p90min}  {hr90min}")
+        print(f"    90 min {p90min}  {hr90min}")
     if activity.peak_120min_power or activity.peak_120min_hr:
-        print(f"120 min    {p120min}  {hr120min}")
+        print(f"    120 min{p120min}  {hr120min}")
     print("           ---------  ---------")
